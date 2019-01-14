b5a99aedea485af3710d94ad7fe8a10d57dcf2b6
wisemuji <wisemuji@gmail.com>
2019-01-15 02:23:27 +0900

    http로 서버 열기, socket.io 모듈 추가

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import express from 'express'
 
 let app = express();
 
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
 
 
 const PORT = 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log('Server on port ', PORT);
 })
