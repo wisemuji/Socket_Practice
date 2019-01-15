da3659eb74d45433e64e152b00b4fe7d6f5cc09c
wisemuji <wisemuji@gmail.com>
2019-01-15 09:53:15 +0900

    HTML 파일 연결

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
 // 요청 -> 반환
 
 // 서버가 값을 보낼 수 있는 방법 중 하나
@@ -16,6 +20,7 @@ let io = require('socket.io')(http);
 
 io.on('connection', (socket) => { // 소켓 접속
     // 이벤트 옆으로 통신 열고, 받을 변수를 콜백 함수 인자값으로
+    console.log('a user connected');
     socket.on('join room', (room) => { // 이벤트 명, 룸 코드(룸 들어가기)
         socket.join(room) // 룸 들어가기
     })
@@ -30,6 +35,10 @@ io.on('connection', (socket) => { // 소켓 접속
             }
             // io.to(room).emit('receive message', user_json); // 프론트에서 서버에게 메세지를 보낼 때
         socket.broadcast.to(room).emit('receive message', user_json)
+    })
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        console.log('msg=' + msg);
     })
     socket.on('disconnect', () => { // 연결이 끊겼을 때
         console.log('user disconnect')
